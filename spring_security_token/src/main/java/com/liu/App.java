2c50beefaa1e40802b1951b3602d8a79e559173a
Liush <442549007@qq.com>
2019-06-06 17:36:26 +0800

    自定义认证Spring security接口

--- spring_security_token/src/main/java/com/liu/App.java
+++ spring_security_token/src/main/java/com/liu/App.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Liush
- * @description  这部分是自定义认证过滤器
+ * @description  这部分是自定义认证过滤器，这部分模拟了用token进行认证（默认是session），结合spring_security
+ * 完成一个基于token验证，和自定义数据库配置url进行鉴权的系统
  * @date 2019/6/6 15:23
  **/
 @SpringBootApplication
