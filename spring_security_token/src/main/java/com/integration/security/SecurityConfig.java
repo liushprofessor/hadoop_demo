52d35603ca94341eee20fd2cbbf64fd9a59a4567
Liush <442549007@qq.com>
2019-06-13 18:12:53 +0800

    spring增加全局异常处理

--- spring_security_token/src/main/java/com/integration/security/SecurityConfig.java
+++ spring_security_token/src/main/java/com/integration/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.integration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,15 +28,18 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 // we don't need CSRF because our token is invulnerable
                 .csrf().disable()
-                //设置异常统一处理
+                //设置认证异常统一处理 捕获AuthenticationException 401
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
-
+                //.exceptionHandling().accessDeniedHandler()设置鉴权异常处理捕获 AccessDeniedException 403
                 // don't create session
+
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 //开启表单验证的话会默认使用表单验证，如果没有认证会自动跳转到登录页
                 //且在过滤器链中在最后会添加UsernamePasswordAuthenticationFilter过滤器
